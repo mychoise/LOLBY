86a09eda5a0631b8976346cb79ce1ddcf59ca7de
mychoise <dklsabin23@gmail.com>
2026-08-09 17:35:59 +0545

    updated backend address on frontend

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,10 +1,7 @@
 import { io } from "socket.io-client";
 
-export const socket = io(
-  "http://localhost:3000",
-  {
-    withCredentials: true,
-    transports: ["polling", "websocket"],
-    autoConnect: true,
-  },
-);
+export const socket = io("https://lolby-production.up.railway.app", {
+  withCredentials: true,
+  transports: ["polling", "websocket"],
+  autoConnect: true,
+});
