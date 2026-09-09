08597d72324c4305038bcfd2b2fea875eafb7b6d
mychoise <dklsabin23@gmail.com>
2026-09-09 22:04:15 +0545

    updated app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,24 +11,24 @@ import { useEffect } from "react";
 
 const App = () => {
   const serverUrl = "http://localhost:3000";
-  useEffect(() => {
-    socket.connect();
-    function onConnect() {
-      console.log("Connected to server");
-      alert("connection sucess");
-    }
-    function onDisconnect() {
-      console.log("Disconnected from server");
-      alert("disconnection");
-    }
-    socket.on("connect", onConnect);
-    socket.on("disconnect", onDisconnect);
+  // useEffect(() => {
+  //   socket.connect();
+  //   function onConnect() {
+  //     console.log("Connected to server");
+  //     alert("connection sucess");
+  //   }
+  //   function onDisconnect() {
+  //     console.log("Disconnected from server");
+  //     alert("disconnection");
+  //   }
+  //   socket.on("connect", onConnect);
+  //   socket.on("disconnect", onDisconnect);
 
-    return () => {
-      socket.off("connect", onConnect);
-      socket.off("disconnect", onDisconnect);
-    };
-  }, [serverUrl]);
+  //   return () => {
+  //     socket.off("connect", onConnect);
+  //     socket.off("disconnect", onDisconnect);
+  //   };
+  // }, [serverUrl]);
   return (
     <div className="bg-[#000000] h-auto min-h-screen w-full text-white">
       <Navbar />
